e012ed4c6e3024c324d00ff2f8ed7c5224d9c63c
fromScratch-ToDev <145133454+fromScratch-ToDev@users.noreply.github.com>
2025-04-30 20:51:17 +0200

    sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,18 @@
+// next-sitemap.config.js
+module.exports = {
+    siteUrl: 'https://peinturesetdessinsdefrancoismalespine.vercel.app',
+    generateRobotsTxt: true,
+    autoLastmod: true,
+    sitemapSize: 7000,
+    trailingSlash: false,
+    generateIndexSitemap: true,
+    exclude: ['/admin'], // si tu veux exclure des routes
+    // active le crawler pour découvrir les routes App Router
+    transform: async (config, path) => {
+      return {
+        loc: path,
+        lastmod: new Date().toISOString(),
+      }
+    },
+  }
+  
