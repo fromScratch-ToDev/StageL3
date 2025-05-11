52e0d1cdfcd905268a8d1d7d222496bd2f1d1789
fromScratch-ToDev <145133454+fromScratch-ToDev@users.noreply.github.com>
2025-05-11 16:14:02 +0200

    Footer pour les portables + ajout gitignore

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -3,12 +3,12 @@ import P from '../Text/P'
 
 function Footer() {
   return (
-    <footer className='p-2.5 self-start flex justify-between w-screen'>
-      <P size='xs' text_fr='2025 © François Malespine.  Tous droits réservés.' text_en='2025 © François Malespine. All rights reserved.' className='ml-0'></P>
-      <div className='px-5 flex gap-5' >
+    <footer className='p-2.5 block md:flex md:flex-row-reverse self-start justify-between w-screen '>
+      <div className='md:px-5 block md:flex gap-5 justify-between ' >
         <Link href="/politique_de_confidentialite"><P size='xs' text_fr='Politique de confidentialité' text_en='Privacy Policy'></P></Link>
         <Link href="/cgu"><P size='xs' text_fr='Conditions d’utilisation' text_en='Terms of Service'></P></Link>
       </div>
+      <P size='xs' text_fr='2025 © François Malespine.  Tous droits réservés.' text_en='2025 © François Malespine. All rights reserved.' className='ml-0'></P>
     </footer>
   )
 }
